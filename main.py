83c54347b87a9f8470a3016836ebe2318cab02d5
ghghghost <attapapa@naver.com>
2023-03-09 19:51:38 +0900

    assignment

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-from datetime import datetime
 
-print "hello git"
+def twoSum(nums, target):
+    
+    break_ = False
+    for i in range(1, len(nums)):
+        for j in range(i):
+            if nums[i] + nums[j] == target:
+                break_check = True
+                listt=[i,j] 
+                return listt
+                break
+        if break_:
+            break
 
-print (datetime.now())
+
+nums = list(map(int, input().split()))
+
+target = int(input())
+
+print(twoSum(nums,target))
